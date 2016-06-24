551d3f6b861a95eb8ce5ef9de5d487264db4ae9a
martinoj <jhonnyx@gmail.com>
2016-06-23 22:56:18 -0300

    Revert "msm: pil: Add module parameter to override proxy vote timeouts"
    This reverts commit f8414c8d9cf051130ab8109f98965afd98f6e233.

--- arch/arm/mach-msm/peripheral-loader.c
+++ arch/arm/mach-msm/peripheral-loader.c
@@ -34,15 +34,6 @@
 
 #include "peripheral-loader.h"
 
-/**
- * proxy_timeout - Override for proxy vote timeouts
- * -1: Use driver-specified timeout
- *  0: Hold proxy votes until shutdown
- * >0: Specify a custom timeout in ms
- */
-static int proxy_timeout_ms = -1;
-module_param(proxy_timeout_ms, int, S_IRUGO | S_IWUSR);
-
 enum pil_state {
 	PIL_OFFLINE,
 	PIL_ONLINE,
@@ -136,10 +127,7 @@ static int pil_proxy_vote(struct pil_device *pil)
 
 static void pil_proxy_unvote(struct pil_device *pil, unsigned long timeout)
 {
-	if (proxy_timeout_ms >= 0)
-		timeout = proxy_timeout_ms;
-
-	if (timeout && pil->desc->ops->proxy_unvote)
+	if (pil->desc->ops->proxy_unvote)
 		schedule_delayed_work(&pil->proxy, msecs_to_jiffies(timeout));
 }
 
@@ -405,11 +393,7 @@ EXPORT_SYMBOL(pil_get);
 static void pil_shutdown(struct pil_device *pil)
 {
 	pil->desc->ops->shutdown(pil->desc);
-	if (proxy_timeout_ms == 0 && pil->desc->ops->proxy_unvote)
-		pil->desc->ops->proxy_unvote(pil->desc);
-	else
-		flush_delayed_work(&pil->proxy);
-
+	flush_delayed_work(&pil->proxy);
 	pil_set_state(pil, PIL_OFFLINE);
 }
 
